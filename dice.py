6ec5bc50258dba1731343c6b390fbe965f9c21b5
Tachibana Kiyomi <me@yuru.me>
2021-09-26 02:48:53 +0900

    move bot to new repository

---
+++ dice.py
@@ -0,0 +1,209 @@
+import secrets
+import enum
+import typing
+
+from pypeg2 import *
+from entities import Entity, RollResult, Span
+
+
+class DiceErrorKind(enum.Enum):
+    ZERO_DIVISION = 'ZERO_DIVISION'
+    ROLL_SYNTAX_ERROR = 'ROLL_SYNTAX_ERROR'
+
+
+class RollError(RuntimeError):
+    pass
+
+
+class Env:
+    def __init__(self, face=20):
+        self.face = face
+
+
+class Number(Symbol):
+    regex = re.compile(r'\d{1,4}')
+
+    def eval(self, *_args):
+        return int(self.name), self.name
+
+
+class Operator(Symbol):
+    display = ''
+
+
+class Add(Operator):
+    regex = re.compile(r'\+')
+    display = '+'
+
+
+class Sub(Operator):
+    regex = re.compile(r'-')
+    display = '-'
+
+
+class Mul(Operator):
+    regex = re.compile(r'[*×]')
+    display = '×'
+
+
+class Div(Operator):
+    regex = re.compile(r'[/÷]')
+    display = '÷'
+
+
+operator = [Add, Sub, Mul, Div]
+
+
+class Dice(Symbol):
+    regex = re.compile(r'\d{0,4}[dD]\d{0,4}')
+
+    def eval(self, env: Env, result_sum=True):
+        match = self.name.split('d')
+        try:
+            counter = int(match[0])
+        except ValueError:
+            counter = 1
+        try:
+            face = int(match[1])
+        except:
+            face = env.face
+
+        if face == 0 or counter == 0:
+            result = [0]
+        elif face == 1:
+            result = [1 for _ in range(counter)]
+        else:
+            result = [secrets.randbelow(face) + 1 for _ in range(counter)]
+        if len(result) > 16:
+            result_text = '={...}'
+        elif counter < 2:
+            result_text = ''
+        else:
+            result_text = '={{{}}}'.format(', '.join(map(str, result)))
+        show = '{}d{}{}'.format(counter, face, result_text)
+        if result_sum:
+            result = sum(result)
+            show += '={}'.format(result)
+        return result, show
+
+
+class Max:
+    dice = None
+    grammar = (
+        ["最大", "max", "MAX", "Max"],
+        [('(', attr('dice', Dice), ')'), ('（', attr('dice', Dice), '）'), attr('dice', Dice)]
+    )
+
+    def eval(self, env):
+        value, text = self.dice.eval(env, result_sum=False)
+        result = max(value)
+        return result, 'max {}'.format(text, result)
+
+
+class Min:
+    dice = None
+    grammar = (
+        ["最小", "min", "Min", "MIN"],
+        [('(', attr('dice', Dice), ')'), ('（', attr('dice', Dice), '）'), attr('dice', Dice)]
+    )
+
+    def eval(self, env):
+        value, text = self.dice.eval(env, result_sum=False)
+        result = min(value)
+        return result, 'min {}'.format(text, min(value))
+
+
+# left recursion!
+# https://bitbucket.org/fdik/pypeg/issues/4/
+class Expr(List):
+    def eval(self, env):
+        value_list = []
+        show_list = []
+
+        for i in self:
+            if not isinstance(i, Operator):
+                v, show = i.eval(env)
+                value_list.append(v)
+                show_list.append(show)
+            else:
+                show_list.append(i.display)
+                value_list.append(i)
+
+        for i, current in enumerate(value_list):
+            if isinstance(current, (Mul, Div)):
+                a = value_list[i - 1]
+                b = value_list[i + 1]
+                if isinstance(current, Mul):
+                    value_list[i + 1] = a * b
+                elif b == 0:
+                    raise RollError(DiceErrorKind.ZERO_DIVISION)
+                else:
+                    value_list[i + 1] = a // b
+                value_list[i] = None
+                value_list[i - 1] = None
+        value_list = list(filter(lambda x: x is not None, value_list))
+        result = value_list[0]
+        for i, current in enumerate(value_list):
+            if isinstance(current, Add):
+                result += value_list[i + 1]
+            elif isinstance(current, Sub):
+                result -= value_list[i + 1]
+        return result, '[{}]={}'.format(' '.join(show_list), result)
+
+
+item = [Dice, Number, Max, Min, ('(', Expr, ')')]
+Expr.grammar = (item, maybe_some(operator, item))
+
+
+class Roll(List):
+    grammar = maybe_some([
+        Expr,
+        re.compile(r'[^。.；，,、…！!？/ ⧸⁄—-【】“”"（）()：:\[\]{}<>《》〔〕『』‘’「」\s]+'),
+        re.compile(r'[。.；，,、…！!？/ ⧸⁄—-【】“”"（）()：:\[\]{}<>《》〔〕『』‘’「」]+'),
+    ])
+
+    def eval_entities(self, env) -> typing.List[Entity]:
+        entities = []
+        for e in self:
+            if isinstance(e, str):
+                entities.append(Span(e))
+            else:
+                value, text = e.eval(env)
+                result_value = value
+                entities.append(RollResult(text, result_value))
+        return entities
+
+    def eval(self, env):
+        expr_count = 0
+        result_value = 0
+        result_text = []
+        for e in self:
+            if isinstance(e, str):
+                result_text.append(e)
+            else:
+                value, text = e.eval(env)
+                result_value = value
+                expr_count += 1
+                result_text.append('<code>{}</code>'.format(text))
+        if expr_count == 0:
+            result_value, text = parse('1d', Dice).eval(env)
+            result_text.insert(0, '<code>{}</code>'.format(text))
+        return result_value, ' '.join(result_text)
+
+
+def roll_entities(text, default_dice_face) -> typing.List[Entity]:
+    env = Env(face=default_dice_face)
+    try:
+        roll_ast = parse(text, Roll)
+    except SyntaxError:
+        raise RollError(DiceErrorKind.ROLL_SYNTAX_ERROR)
+    return roll_ast.eval_entities(env)
+
+
+def roll(text, default_dice_face):
+    env = Env(face=default_dice_face)
+    try:
+        roll_ast = parse(text, Roll)
+    except SyntaxError:
+        raise RollError(DiceErrorKind.ROLL_SYNTAX_ERROR)
+    return roll_ast.eval(env)
---
+++ entities.py
@@ -0,0 +1,225 @@
+import re
+from typing import List, Optional
+
+
+def escape(text: str) -> str:
+    text = text.replace('&', '&amp;')
+    text = text.replace('<', '&lt;')
+    text = text.replace('>', '&gt;')
+    return text
+
+
+class Entity:
+    kind = 'none'
+    value = None
+
+    def object(self) -> dict:
+        obj = self.__dict__
+        obj['kind'] = self.kind
+        return obj
+
+    def telegram_html(self):
+        return ''
+
+    @staticmethod
+    def from_object(obj: dict) -> 'Entity':
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return '<{} | {}>'.format(self.kind, self.value)
+
+
+class Entities:
+    list: List[Entity]
+
+    def __init__(self, li=None):
+        self.list = li or []
+
+    def to_object(self) -> List[dict]:
+        return make_entities_object(self.list)
+
+    @staticmethod
+    def from_html(html: str) -> 'Entities':
+        new = Entities()
+        new.list = convert_to_entities(html)
+        return new
+
+    @staticmethod
+    def from_object(xs: List[dict]) -> 'Entities':
+        return Entities(list(map(object_to_entity, xs)))
+
+    def telegram_html(self) -> str:
+        return entities_to_telegram_html(self.list)
+
+
+class Span(Entity):
+    kind = 'span'
+
+    def __init__(self, text):
+        self.value = text
+
+    def telegram_html(self):
+        return escape(self.value)
+
+    @staticmethod
+    def from_object(obj: dict) -> 'Span':
+        return Span(obj['value'])
+
+
+class Character(Entity):
+    kind = 'character'
+
+    def __init__(self, character: str, player_id: int, full_name: str):
+        self.value = character
+        self.player_id = player_id
+        self.full_name = full_name
+
+    def telegram_html(self):
+        return '<b>{}</b>'.format(escape(self.value))
+
+    @staticmethod
+    def from_object(obj: dict) -> 'Character':
+        return Character(obj['value'], obj['player_id'], obj['full_name'])
+
+
+class Me(Character):
+    kind = 'me'
+
+    @staticmethod
+    def from_object(obj: dict) -> 'Me':
+        return Me(obj['value'], obj['player_id'], obj['full_name'])
+
+
+class Bold(Entity):
+    kind = 'bold'
+
+    def __init__(self, text):
+        self.value = text
+
+    def telegram_html(self):
+        return '<b>{}</b>'.format(escape(self.value))
+
+    @staticmethod
+    def from_object(obj: dict) -> 'Bold':
+        return Bold(obj['value'])
+
+
+class Code(Entity):
+    kind = 'code'
+
+    def __init__(self, text):
+        self.value = text
+
+    def telegram_html(self):
+        return '<code>{}</code>'.format(escape(self.value))
+
+    @staticmethod
+    def from_object(obj: dict) -> 'Code':
+        return Code(obj['value'])
+
+
+class RollResult(Entity):
+    kind = 'roll'
+
+    def __init__(self, text, result=None):
+        self.value = text
+        self.result = result
+
+    def telegram_html(self):
+        return ' <code>{}</code> '.format(escape(self.value))
+
+    @staticmethod
+    def from_object(obj: dict) -> 'RollResult':
+        return RollResult(obj['value'], obj['result'])
+
+
+class LoopResult(Entity):
+    kind = 'loop-roll'
+
+    def __init__(self, rolled: List[int]):
+        self.rolled = rolled
+
+    def telegram_html(self):
+        counter_6 = self.rolled.count(6)
+        counter_all = len(self.rolled)
+        rolled_text = ', '.join(map(str, self.rolled))
+        return ' <code>({}/{}) [{}]</code> '.format(counter_6, counter_all, rolled_text)
+
+    @staticmethod
+    def from_object(obj: dict) -> 'LoopResult':
+        return LoopResult(obj['rolled'])
+
+
+class CocResult(Entity):
+    kind = 'coc-roll'
+
+    def __init__(self, rolled: int, level: str,
+                 modifier_name: Optional[str], rolled_list: List[int]):
+        self.rolled = rolled
+        self.level = level
+        self.modifier_name = modifier_name
+        self.rolled_list = rolled_list
+
+        self.value = level
+
+    def telegram_html(self):
+        result = '<code>{rolled}</code> {level}'.format(
+            rolled=self.rolled, level=self.level,
+        )
+        if self.modifier_name:
+            result += '\n\n{modifier_name}: <code>{rolled_list}</code>'.format(
+                modifier_name=self.modifier_name,
+                rolled_list=', '.join(map(str, self.rolled_list)),
+            )
+        return result
+
+    @staticmethod
+    def from_object(obj: dict) -> 'CocResult':
+        return CocResult(obj['rolled'], obj['level'], obj['modifier_name'], obj['rolled_list'])
+
+
+CODE_REGEX = re.compile(r'<(code)>(.+?)</code>')
+BOLD_REGEX = re.compile(r'<(b)>(.+?)</b>')
+
+
+def convert_to_entities(content: str) -> List[Entity]:
+    matches = list(CODE_REGEX.finditer(content))
+    matches.extend(BOLD_REGEX.finditer(content))
+    matches.sort(key=lambda m: m.start())
+    entities = []
+    last_index = 0
+    for match in matches:
+        tag = match.group(1)
+        text = match.group(2)
+        start = match.start()
+        if start < last_index:
+            continue
+        segment = content[last_index:start]
+        if segment:
+            entities.append(Span(segment))
+        last_index = match.end()
+        if tag == 'code':
+            entities.append(Code(text))
+        elif tag == 'b':
+            entities.append(Bold(text))
+    if last_index < len(content) - 1:
+        entities.append(Span(content[last_index:]))
+    return entities
+
+
+def entities_to_telegram_html(entities: List[Entity]) -> str:
+    html = ''.join(map(lambda e: e.telegram_html(), entities))
+    return html.strip()
+
+
+def make_entities_object(entities: List[Entity]):
+    return list([entity.object() for entity in entities])
+
+
+def object_to_entity(obj: dict) -> Optional[Entity]:
+    kind = obj.get('kind', '')
+    for E in (Span, Character, Me, Bold, Code, RollResult, LoopResult, CocResult):
+        if E.kind == kind:
+            return E.from_object(obj)
+    return None
+
