80440cab945a8165a4339a9c653b4f892d39f19e
Tachibana Kiyomi <me@yuru.me>
2021-09-29 13:30:22 +0900

    update flake

--- flake.nix
+++ flake.nix
@@ -1,31 +1,61 @@
 {
   description = "Telegram Dice Bot";
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs";
     utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs, utils }: (utils.lib.eachSystem [ "x86_64-linux" ] (system:
-  let
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system: 
+  let 
     pkgs = nixpkgs.legacyPackages.${system};
-    pythonEnv = pkgs.python3.withPackages(packages: with packages; [
-      python-telegram-bot python-dotenv pypeg2 faker
-    ]);
     telegram-dice-bot = with pkgs.python3Packages; buildPythonPackage {
       pname = "telegram-dice-bot";
       version = "1.0";
       propagatedBuildInputs = [ python-telegram-bot python-dotenv pypeg2 faker ];
       src = ./.;
     };
+    python = pkgs.python3.withPackages(ps: [ telegram-dice-bot ]);
   in {
+    overlay = prev: self: {
+      inherit telegram-dice-bot;
+    };
     packages = {
-      pythonEnv = pythonEnv;
-      telegram-dice-bot = telegram-dice-bot;
+      inherit telegram-dice-bot;
     };
     defaultApp = utils.lib.mkApp {
       drv = telegram-dice-bot;
       exePath = "/bin/bot.py";
     };
-    defaultPackage = telegram-dice-bot; # If you want to juist build the environment
-    devShell = pythonEnv.env; # We need .env in order to use `nix develop`
-  }));
+    nixosModule = { config, lib, ... }:
+    with lib;
+    let 
+      cfg = config.services.telegram-dice-bot;
+    in {
+      options = {
+        services.telegram-dice-bot = {
+          enable = mkEnableOption "telegram dice bot service";
+          token = mkOption { type = types.str; };
+        };
+      };
+      config = mkIf cfg.enable {
+        systemd.services.telegram-dice-bot = {
+          enable = true;
+          requires = [ "network.target" ];
+          wantedBy = [ "multi-user.target" ];
+          serviceConfig = {
+            Type = "simple";
+            User = "telegram-dice-bot";
+            Group = "telegram-dice-bot";
+            DynamicUser = true;
+            MemoryMax = "128M";
+          };
+          environment = {
+            TOKEN = cfg.token;
+          };
+          script = "${telegram-dice-bot}/bin/bot.py";
+        };
+      };
+    };
+    defaultPackage = telegram-dice-bot;
+    devShell = python.env;
+  });
 }
